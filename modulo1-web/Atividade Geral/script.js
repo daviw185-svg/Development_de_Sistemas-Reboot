232db75b37851b2d454f45589e3eacb1ad028943
daviw185-svg <daviw185@gmail.com>
2026-08-19 14:27:05 -0300

    Busca de usuário de GitHub - Finalizado

--- modulo1-web/Atividade Geral/script.js
+++ modulo1-web/Atividade Geral/script.js
@@ -1,1 +1,12 @@
-const btnBuscar = document.getElementById('buscar');
+document.getElementById('buscar').addEventListener('click', () => {
+    let nome = document.getElementById('usuario').value; //Guardar a variável
+    fetch(`https://api.github.com/users/${nome}`)
+    .then((r) => r.json())
+    .then((user) => { //Juntar script com HTML
+        document.getElementById('perfil').innerHTML = `
+        <img src="${user.avatar_url}" width="120"
+        <h3>${user.name || "Sem nome"}</h3>
+        <p>${user.bio || "Bio indisponível"}</p>`;
+    });
+});    
+    
